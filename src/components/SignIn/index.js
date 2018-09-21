4507800ed449e271ec9071968358b3b3d8afb41f
aloysen <aloysen951@gmail.com>
2018-09-21 13:34:17 +0530

    intermediary additioins more to come today soon

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,137 +1,76 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
-import { SignUpLink } from '../SignUp';
 
-import { PasswordForgetLink } from '../PasswordForget';
 import { auth } from '../../firebase';
 import * as routes from '../../constants/routes';
 
 
-const SignInPage = ({ history }) =>
-  <div>
-    <SignInForm history={history} />
-    
+const SignInModal = ({ history }) =>
+<div>
+  <SignInForm history={history} />
   
-  </div>
+
+</div>
 
 const updateByPropertyName = (propertyName, value) => () => ({
   [propertyName]: value,
 });
 
-const INITIAL_STATE = {
-  email: '',
-  password: '',
-  error: null,
-  finished: false,
-  stepIndex: 0,
-};
-
 class SignInForm extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = { ...INITIAL_STATE };
-  }
+handleGoogleSignin = (event) => {
+ auth.doGoogleSignIn().then( ()=>{
+   this.props.history.push("/home");
 
-
+ }).catch( (reason)=> {
+   console.log("error login ::"+ reason);
+ })
   
-  onEmailSignIn = (event) => {
-    const {
-      email,
-      password,
-    } = this.state;
-
-    const {
-      history,
-    } = this.props;
-
-    auth.doSignInWithEmailAndPassword(email, password)
-      .then(() => {
-        this.setState(() => ({ ...INITIAL_STATE }));
-        this.props.history.push("/home");
-      })
-      .catch(error => {
-        this.setState(updateByPropertyName('error', error));
-        console.log("sign in error")
-      });
-
-    event.preventDefault();
-  }
-
-  handleGoogleSignin = (event) => {
-   auth.doGoogleSignIn().then( ()=>{
-     this.props.history.push("/home");
-
-   }).catch( (reason)=> {
-     console.log("error login ::"+ reason);
-   })
-    
-  }
-
-  render() {
-    const {
-      email,
-      password,
-      error,
-
-    } = this.state;
-
-    
+}
 
     return (
       
-        <form onSubmit={this.onEmailSignIn} className="w3-display-middle w3-large">
-        <div className="w3-container w3-card w3-white w3-round w3-margin">
-    
-        <h4> Sign in</h4><br/>
-        <hr className="w3-clear"/>
-    <h4> { error && <p>{error.message}</p> } </h4>
-    <label>enter the unique ID </label>
-            <input
-              value={email}
-                onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
-                 
-            />
-            <br/>  
-            <label>eneter the password that you had set </label>
-            
-            <input
-        value={password}
-        onChange={event => this.setState(updateByPropertyName('password', event.target.value))}
-          
-          type="password"
-        />
-        <br/>
-        <button className="w3-button w3-theme-d1 w3-margin-bottom"
-          
-          
-          type ="submit"
-          onSubmit ={ this.onEmailSignIn }
-        >See you </button>
-           <PasswordForgetLink />
-          <SignUpLink />
-        </div>
+        <div id="LoginModal" className="modal modal-content animate">
+-------------------------------
+        <div class="imgcontainer">
+      <img src="img_avatar2.png" alt="Avatar" class="avatar">
+    </div>
 
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+      ------------------
+      <form onSubmit={this.handleGoogleSignin} >
+      <button type="submit">Login With Google</button>
+      </form>
+      ----------------------- MOVE TO CENTRE
+      ------------------
+      <form onSubmit={this.handleFacebookSignin} >
+      <button type="submit">Login With Facebook</button>
+      </form>
+      ----------------------- MOVE TO CENTRE
+      <label>
+        <Link to="routes.TERMS" name="T&C"> Terms of Service </Link>
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+      <button type="button" onclick="document.getElementById('LoginModal').style.display='none'" 
+        class="cancelbtn">Return to news</button>
+      <span class="psw"><h4> { error && <p>{error.message}</p> } </h4></span>
+    </div>
+    ------------------------
         
-          <div className="w3-container w3-card w3-white w3-round w3-margin">
-        <h4>Alternate Sign in</h4><br/>
-        <hr className="w3-clear"/>
-        <button type="button"
-        onClick={this.handleGoogleSignin} 
-        primary={"true"}
-        className="w3-button w3-theme-d2 w3-margin-bottom"> 
-        Login with google</button>
-
-          
         </div>
-          </form>
+          
     );
   }
 }
 
-export default withRouter(SignInPage);
+export default withRouter(SignInModal);
 
-export {
-  SignInForm,
-};
+
